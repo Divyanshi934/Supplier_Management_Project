24ddeb14874f2801dfc45d1695bd32ec9baf381e
Divyanshi934 <52443706+Divyanshi934@users.noreply.github.com>
2021-02-22 22:54:57 +0530

    Delete pool.js

--- pool.js
+++
@@ -1,9 +0,0 @@
-var mysql=require('mysql')
-var pool=mysql.createPool(
-    {
-       host:'localhost',port:3307,
-       user:'root',password:'@divyaBTS7',
-       database:'supplier',connectionLimit:100,
-       multipleStatements:true 
-    });
-    module.exports=pool;
